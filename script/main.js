4f65c454d266ada5fb062466aea451e8b693e704
LunaDoro <dorothee.valentyn@gmail.com>
2024-11-27 10:02:10 +0100

    avancement

--- script/main.js
+++ script/main.js
@@ -3,6 +3,8 @@ const left_btn = document.querySelectorAll(".btn_projets");
 const right_btn = document.querySelectorAll(".btn_competences");
 const bottom_btn =document.querySelectorAll(".btn_parcours")
 const high_btn = document.querySelectorAll(".btn_contact");
+const fleurs = document.querySelectorAll('.fleur');
+
 
 const movable = document.querySelector(".movable");
 
@@ -23,7 +25,18 @@ right_btn.forEach(btn => {
     btn.addEventListener("click", function(){
         movable.style.transform="translateX(-100vw)";
         console.log(movable.style.transform);
+        fleurs.forEach(fleur => {
+            fleur.classList.remove('poussee'); // Retirer toute ancienne animation
+        });
+    
+        // Ajouter un délai pour appliquer la classe .poussee
+        setTimeout(() => {
+            fleurs.forEach(fleur => {
+                fleur.classList.add('poussee'); // Lancer l'animation
+            });
+        }, 100); // Délai pour permettre de réinitialiser visuellement
     });
+    // Réinitialiser les fleurs pour permettre de rejouer l'animation
 });
 
 
@@ -38,5 +51,13 @@ high_btn.forEach(btn => {
     btn.addEventListener("click", function(){
         movable.style.transform="translateY(100vh)";
         console.log(movable.style.transform);
+    });
+});
+
+document.querySelectorAll('.fleur-zone').forEach(zone => {
+    zone.addEventListener('click', (e) => {
+        e.preventDefault();
+        const section = zone.getAttribute('data-section');
+        document.querySelector(`#${section}`).scrollIntoView({ behavior: 'smooth' });
     });
 });
